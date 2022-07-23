d89fc84d0cea7e3d33040010bcd4c250230b0357
Allef Andre de Souza Moura <allefsouza@hotmail.com>
2022-07-22 22:56:33 -0300

    finalizado

--- src/Components/Home/products.js
+++ src/Components/Home/products.js
@@ -1,30 +1,46 @@
-import React from 'react'
+import brinquedo from "./Assets/brinquedo tematico.png"
+import meteorito from "./Assets/meteorito-lunar.jpeg"
+import nave from "./Assets/nave.png"
+import roupas from "./Assets/roupa.jpg"
+import viajem from "./Assets/viajem.jpeg"
+import satelite from "./Assets/satelite.jpg"
 
-const products = () => {
-   const productsList = [
+export const products =  [
     {id:1,
     product:'Brinquedo Planetas' ,
-    price:50},
-    {id:1,
+    price:50, 
+    quantidade:1,
+    img: brinquedo},
+
+    {id:2,
     product:'Meteorito' ,
-    price:5000},
-    {id:1,
+    price:5000, 
+    quantidade:1,
+    img:meteorito},
+
+    {id:3,
     product:'Nave' ,
-    price:350 },
-    {id:1,
+    price:350,  
+    quantidade:1,
+    img:nave },
+
+    {id:4,
     product:'Roupa Espacial' ,
-    price:2000},
-    {id:1,
+    price:2000,  
+    quantidade:1,
+    img: roupas},
+
+    {id:5,
     product:'Viajem Monstrona' ,
-    price:10},
-    {id:1,
+    price:10,
+    quantidade:1,
+    img:viajem},
+
+    {id:6,
     product:'Satelite' ,
-    price:99 }
+    price:99, 
+    quantidade:1,
+    img:satelite}
    ]
   
-    return (
-
-  )
-}
-
-export default products
+    
